35f5a72a7e14e008bc3dd785b72b5030d88e31d0
vlad saling <vladislavsaling@gmail.com>
2021-03-09 11:04:16 +0100

    firebase udpate

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,4 +1,8 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_apiKey,
@@ -17,3 +21,6 @@ export const auth = firebaseApp.auth(); // the firebase auth namespace
 export const storageKey = 'adGKUgxtIT1rngYvHtk4VzTXsfDT0Huz';
 // eslint-disable-next-line no-undef
 export const isAutheticated = () => !!auth.currentUser || !!localStorage.getItem(storageKey);
+
+export default firebase;
+
