098af79ebe116b535457eca14122882447f4e3fe
Vladislav Šaling <vladislav.saling@sk.ibm.com>
2021-04-30 15:25:41 +0200

    sentry test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+/* Sentry */
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
 /* Redux */
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -10,6 +14,17 @@ import store from './redux/store';
 import App from './containers/App';
 
 import registerServiceWorker from './registerServiceWorker';
+
+Sentry.init({
+  dsn: 'https://a5f20620a0994372941124e0b6b519f3@o592704.ingest.sentry.io/5741263',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 
 /* JUST FOR DEVELOPMENT */
 // Log the initial state
